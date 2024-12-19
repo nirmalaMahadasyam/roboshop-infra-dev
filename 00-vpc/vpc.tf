32c4b8b24728ca5c657d1bf007b00b7471766315
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-12-19 22:14:24 +0530

    roboshop infra dev

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
     source = "git::https://github.com/nirmalaMahadasyam/terraform-aws-vpc.git?ref=main"
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
-    environment = var.environment
+    environment = var.environment_name
     common_tags = var.common_tags
     public_subnet_cidr = var.public_subnet_cidr
     private_subnet_cidr = var.private_subnet_cidr
