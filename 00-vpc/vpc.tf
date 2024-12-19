5e22ebdef63c238acac0a7affb3ca8bdcf9687b7
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-12-19 21:46:24 +0530

    roboshop infra dev

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -5,9 +5,9 @@ module "vpc" {
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
-    public_subnet_cidrs = var.public_subnet_cidrs
-    private_subnet_cidrs = var.private_subnet_cidrs
-    database_subnet_cidrs = var.database_subnet_cidrs
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
     is_peering_required = var.is_peering_required
 }
 
